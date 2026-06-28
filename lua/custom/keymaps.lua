632fe3aae338e7c7af71dd78b835ade26ab464f9
Dominik <wezik@users.noreply.github.com>
2026-06-28 16:26:46 +0200

    Disable kulala tree-sitter management

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -12,3 +12,37 @@ vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 -- Neotree override
 vim.keymap.set('n', '<leader>te', '<cmd>Neotree reveal<CR>')
+
+-- Harpoon
+local harpoon = require 'harpoon'
+
+vim.keymap.set('n', '<leader><Tab>', function()
+    harpoon:list():add()
+end)
+
+vim.keymap.set('n', '<leader>H', function()
+    harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+vim.keymap.set('n', '<leader>1', function()
+    harpoon:list():select(1)
+end)
+
+vim.keymap.set('n', '<leader>2', function()
+    harpoon:list():select(2)
+end)
+
+vim.keymap.set('n', '<leader>3', function()
+    harpoon:list():select(3)
+end)
+
+vim.keymap.set('n', '<leader>4', function()
+    harpoon:list():select(4)
+end)
+
+vim.keymap.set('n', '<leader>5', function()
+    harpoon:list():select(5)
+end)
+
+vim.keymap.set('n', '<leader>`', '<cmd>b#<CR>')
+
